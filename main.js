28efc69d4440f4e8a9abf1717bf44d44a7b1fd40
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2021-10-13 10:44:12 +0800

    init main code

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('test main')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening at port ${ port }`)
+})
