cf6346944aa2503fa43f4bf76a23f3921043c214
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2021-10-22 23:13:27 +0800

    modify structure

---
+++ controllers/routes.js
@@ -0,0 +1,22 @@
+// app.get('/:mainRoute', async (req, res) => {
+//     return await jsonRespHandler(req, res)
+//         .execute(props => {
+//             // throw({code: 500, message: 'na daot'})
+//             // throw({code: 404, message: 'na daot'})
+//             return {processResult: 'response data', props}
+//         })
+// })
+
+// app.post('/postTest/:endRoute', async (req, res) => {
+//     return await jsonRespHandler(req, res)
+//         .execute(props => {
+//             // throw({code: 500, message: 'na daot'})
+//             // throw({code: 404, message: 'na daot'})
+//             return {processResult: 'response data', props}
+//         })
+// })
+
+// public routes
+
+
+// secured routes
--- main.js
+++ main.js
@@ -15,23 +15,8 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/:mainRoute', async (req, res) => {
-  return await jsonRespHandler(req, res)
-    .execute(props => {
-      // throw({code: 500, message: 'na daot'})
-      // throw({code: 404, message: 'na daot'})
-      return {processResult: 'response data', props}
-    })
-})
 
-app.post('/postTest/:endRoute', async (req, res) => {
-  return await jsonRespHandler(req, res)
-    .execute(props => {
-      // throw({code: 500, message: 'na daot'})
-      // throw({code: 404, message: 'na daot'})
-      return {processResult: 'response data', props}
-    })
-})
+
 
 app.listen(port, () => {
   // express log
