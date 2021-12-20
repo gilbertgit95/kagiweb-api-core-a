145e4e7e1ca8eeb43d9d94f116a0ca04e69a8e63
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2021-12-20 13:42:51 +0800

    temporarily remove controllers

--- controllers/graphQl/logs/index.js
+++
@@ -1,9 +0,0 @@
-const types = require('./types');
-const queries = require('./queries');
-const mutations = require('./mutations');
-
-module.exports = {
-    types,
-    queries,
-    mutations
-}
--- controllers/restApi/tests/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const {
-    getTests
-} = require('./tests');
-
-router.get('/tests/:vals', getTests)
-
-module.exports = router
