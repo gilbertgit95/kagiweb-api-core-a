f55e861d00b10e06fdcbd2c905aea41c2eb9687f
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2021-12-24 01:32:42 +0800

    update single account controller

--- controllers/restapi/accounts/singleAccount.js
+++ controllers/restapi/accounts/singleAccount.js
@@ -3,42 +3,67 @@ const { jsonRespHandler } = require('../../../utilities/responseHandler');
 
 const {
     Sequelize,
-    sequelize
+    sequelize,
+    Account
 } = require('../../../dataSource/models');
+
+const {
+    getItem,
+    createItem,
+    updateItem,
+    deleteItem
+} = require('../../../utilities/queryHandler');
 
 const getAccount = async (req, res) => {
     return await jsonRespHandler(req, res)
-        .execute(props => {
-            // throw({code: 500, message: 'na daot'})
-            // throw({code: 404, message: 'na daot'})
-            return {}
+        .execute(async (props) => {
+            let id = props.params.id
+
+            return await getItem(Account, id)
         })
 }
 
 const createAccount = async (req, res) => {
     return await jsonRespHandler(req, res)
-        .execute(props => {
-            // throw({code: 500, message: 'na daot'})
-            // throw({code: 404, message: 'na daot'})
-            return {}
+        .execute(async (props) => {
+
+            let accountData = props.body
+
+            return await createItem(Account, accountData)
         })
 }
 
 const updateAccount = async (req, res) => {
     return await jsonRespHandler(req, res)
-        .execute(props => {
-            // throw({code: 500, message: 'na daot'})
-            // throw({code: 404, message: 'na daot'})
-            return {}
+        .execute(async (props) => {
+            let id = props.params.id
+            let accountdata = props.body
+            let accountItem = {...accountdata, ...{ id }}
+
+            return await updateItem(
+                // model
+                Account,
+                // update data
+                accountItem,
+                // setter function
+                (accountModel, accountData) => {
+
+                    if (accountData.roleId)                 accountModel['roleId'] = accountData.password
+                    if (accountData.fullname)               accountModel['fullname'] = accountData.fullname
+                    if (accountData.disableAccount)         accountModel['disableAccount'] = accountData.disableAccount
+
+                    return accountModel
+                }
+            )
         })
 }
 
 const deleteAccount = async (req, res) => {
     return await jsonRespHandler(req, res)
-        .execute(props => {
-            // throw({code: 500, message: 'na daot'})
-            // throw({code: 404, message: 'na daot'})
-            return {}
+        .execute(async (props) => {
+            let id = props.params.id
+
+            return await deleteItem(Account, id)
         })
 }
 
