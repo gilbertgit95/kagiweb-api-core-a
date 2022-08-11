28aa309ea21bfe1bb24f63bb30e16096c8dc8b81
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-08-12 06:54:23 +0800

    add todo in login endpoint

--- controllers/restapi/auth/auth.js
+++ controllers/restapi/auth/auth.js
@@ -9,6 +9,19 @@ const {
 const login = async (req, res) => {
     return await jsonRespHandler(req, res)
         .execute(props => {
+            // get username and password from the request
+
+            // check if the user using the credential
+
+            // if the user did exists
+            // generate authorization token
+
+            // if user does not exist
+            // return 401 or Unauthorized status
+
+            // or else if error uccured during the process
+            // return 500 or Internal server error
+
             // throw({code: 500, message: 'na daot'})
             // throw({code: 404, message: 'na daot'})
             return {}
