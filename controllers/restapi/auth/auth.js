52e7423c1e089a2e4b3e43d621ba71d244c2e619
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-08-13 18:36:18 +0800

    implement login endpoint

--- controllers/restapi/auth/auth.js
+++ controllers/restapi/auth/auth.js
@@ -1,31 +1,57 @@
 const moment = require('moment');
+
+const encryptionHandler = require('../../../utilities/encryptionHandler');
 const { jsonRespHandler } = require('../../../utilities/responseHandler');
+const { getItem } = require('../../../utilities/queryHandler');
 
 const {
+    Account,
     Sequelize,
     sequelize
 } = require('../../../dataSource/models');
 
 const login = async (req, res) => {
     return await jsonRespHandler(req, res)
-        .execute(props => {
+        .execute(async (props) => {
             // get username and password from the request
-            console.log('props: ', props)
+            let { username, password } = props.body
+            let accessToken = null
 
-            // check if the user using the credential
+            // check the credential existed on the request
+            if (!(password && username)) {
+                throw({
+                    message: 'Bad request, missing credential',
+                    code: 400,
+                })
+            }
+
+            // check if the user really exist and
+            // also if the user is using the password
+            let passMatched = false
+            let user = await Account.findOne({ where: { username }})
+            if (user && user.password) {
+                passMatched = await encryptionHandler.verifyTextToHash(
+                    password,
+                    user.password
+                )
+            }
 
             // if the user did exists
             // generate authorization token
-
-            // if user does not exist
+            // else if user does not exist
             // return 401 or Unauthorized status
+            if (user && passMatched) {
+                accessToken = encryptionHandler.generateJWT({
+                    username
+                })
+            } else {
+                throw({
+                    message: 'Bad request, wrong username or password',
+                    code: 400,
+                })
+            }
 
-            // or else if error uccured during the process
-            // return 500 or Internal server error
-
-            // throw({code: 500, message: 'na daot'})
-            // throw({code: 404, message: 'na daot'})
-            return {}
+            return { accessToken }
         })
 }
 
