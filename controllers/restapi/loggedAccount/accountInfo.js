4d4e78e9759b87c60247e10c1f2f34fbcddff76d
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-08-19 23:36:46 +0800

    tod steps for update accountCred and accountProfile

--- controllers/restapi/loggedAccount/accountInfo.js
+++ controllers/restapi/loggedAccount/accountInfo.js
@@ -22,6 +22,26 @@ const getAccount = async (req, res) => {
 const updateAccountCred = async (req, res) => {
     return await jsonRespHandler(req, res)
         .execute(async (props) => {
+            // get the required action type and parameters
+            // action type
+            // let actionType = 'password' // password || emails || phones
+
+            // parameters
+            // for password change:
+            // let currentPassword =
+            // let newPasword =
+
+            // for emails change:
+            // let primaryEmail = 
+            // let secondaryEmail = 
+
+            // for phone numbers change:
+            // let primaryPhone = 
+            // let secondaryPhone =
+
+            // base on the action type
+            // execute the right process
+
             return req.account
         })
 }
@@ -29,6 +49,33 @@ const updateAccountCred = async (req, res) => {
 const updateAccountProfile = async (req, res) => {
     return await jsonRespHandler(req, res)
         .execute(async (props) => {
+            // set this fields
+            // basic profile:
+            // - profilePhoto
+            // - gender
+            // - nickname
+            // - firstname
+            // - middlename
+            // - lastname
+
+            // advance profile:
+            // - nationality
+            // - birthDate
+            // - birthPlace
+            // - homeAddress
+            // - website
+            // - bio
+
+            // work related:
+            // - jobTitle
+            // - companyName
+            // - companyDesc
+            // - companyIndustryType
+            // - companyEmail
+            // - companyNumber
+            // - companyWebsite
+            // - companyAddress
+
             return req.account
         })
 }
