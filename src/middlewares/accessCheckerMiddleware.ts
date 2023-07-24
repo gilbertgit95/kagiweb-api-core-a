d95741baa568d6fb3e8379a7719d1dcd750444fd
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-07-24 12:55:05 +0800

    update logs

--- src/middlewares/accessCheckerMiddleware.ts
+++ src/middlewares/accessCheckerMiddleware.ts
@@ -3,6 +3,7 @@ import { errorLogsColl, combinedLogsColl } from '../utilities/logger'
 class AccessChecker {
     public async middleware(req:any, res:any, next:any) {
         console.log('AccessChecker')
+        console.log('Authorization: ', req.headers.authorization)
         combinedLogsColl.log({level: 'info', message: 'access checker'})
         errorLogsColl.log({level: 'error', message: 'access checker'})
         next()
