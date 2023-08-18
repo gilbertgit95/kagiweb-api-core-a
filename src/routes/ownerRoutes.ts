a738ce458dc98aa41db0dc1780603ee48ebde11e
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-08-19 03:31:00 +0800

    update initial owner routes

--- src/routes/ownerRoutes.ts
+++ src/routes/ownerRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 // import {AppRequest } from '../utilities/globalTypes'
 import Config from '../utilities/config'
+import routerIdentity from '../utilities/routerIdentity'
 // import ErrorHandler from '../utilities/errorHandler'
 // import authController from '../controllers/authController'
 
@@ -10,9 +11,130 @@ import Config from '../utilities/config'
 const env = Config.getEnv()
 const router = express.Router()
 
+// route to fetch the owner data of the current signed in user
 router.get(env.RootApiEndpoint + 'owner', async (req, res) => {
 
     return res.json({})
 })
 
+// routes for owner user info
+router.get(env.RootApiEndpoint + 'owner/userInfo', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/userInfo', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/userInfo/:id', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/userInfo/:id', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/userInfo/:id', async (req, res) => {
+    return res.json({})
+})
+
+// routes for owner contact info
+router.get(env.RootApiEndpoint + 'owner/contactInfo', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/contactInfo', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/contactInfo/:id', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/contactInfo/:id', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/contactInfo/:id', async (req, res) => {
+    return res.json({})
+})
+
+// routes for owner roles
+router.get(env.RootApiEndpoint + 'owner/roles', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/roles', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/roles/:id', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/roles/:id', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/roles/:id', async (req, res) => {
+    return res.json({})
+})
+
+// routes for owner passwords
+router.get(env.RootApiEndpoint + 'owner/passwords', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/passwords', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/passwords/:id', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/passwords/:id', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/passwords/:id', async (req, res) => {
+    return res.json({})
+})
+
+// routes for owner limited transactions
+router.get(env.RootApiEndpoint + 'owner/limitedtransaction', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/limitedtransaction', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/limitedtransaction/:id', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/limitedtransaction/:id', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/limitedtransaction/:id', async (req, res) => {
+    return res.json({})
+})
+
+// routes for owner devices
+router.get(env.RootApiEndpoint + 'owner/devices', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/devices', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/devices/:id', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/devices/:id', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/devices/:id', async (req, res) => {
+    return res.json({})
+})
+
+// routes for owner device access token
+router.get(env.RootApiEndpoint + 'owner/devices/:deviceId/tokens', async (req, res) => {
+    return res.json({})
+})
+router.post(env.RootApiEndpoint + 'owner/devices/:deviceId/tokens', async (req, res) => {
+    return res.json({})
+})
+router.get(env.RootApiEndpoint + 'owner/devices/:deviceId/tokens/:tokenId', async (req, res) => {
+    return res.json({})
+})
+router.put(env.RootApiEndpoint + 'owner/devices/:deviceId/tokens/:tokenId', async (req, res) => {
+    return res.json({})
+})
+router.delete(env.RootApiEndpoint + 'owner/devices/:deviceId/tokens/:tokenId', async (req, res) => {
+    return res.json({})
+})
+
+routerIdentity.addAppRouteObj(router)
 export default router
