2efcc6dfa0996001d2639726ff9cfa86a22f4092
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-10-16 09:35:34 +0800

    update workspace contetrollers

--- src/controllers/workspaceController.ts
+++ src/controllers/workspaceController.ts
@@ -1,99 +1,64 @@
 import DataCache from '../utilities/dataCache'
-import RoleModel, { IRole, IRoleQuery } from '../dataSource/models/roleModel'
+// import RoleModel, { IWorkspace, IWorkspaceQuery } from '../dataSource/models/roleModel'
+import WorkspaceModel, { IWorkspace, IUserRef } from '../dataSource/models/workspaceModel'
 import DataRequest, { IListOutput, IPgeInfo } from '../utilities/dataQuery'
 // import Config from '../utilities/config'
 
 // const env = Config.getEnv()
 
-class RoleController {
+class WorkspaceController {
     public cachedData:DataCache
     public request:DataRequest
 
     constructor() {
-        this.cachedData = new DataCache(RoleModel, { stdTTL: 30, checkperiod: 15 })
-        this.request = new DataRequest(RoleModel)
+        this.cachedData = new DataCache(WorkspaceModel, { stdTTL: 30, checkperiod: 15 })
+        this.request = new DataRequest(WorkspaceModel)
     }
 
-    public async getMappedRole(id:string):Promise<IRole|null> {
-        const rolesMap = await this.getRolesMap()
-
-        if (rolesMap[id]) return rolesMap[id]
-
-        return null
-    }
-
-    public async getRolesMap():Promise<{[key:string]:IRole}> {
-        const allRoles = await this.getAllRoles()
-
-        return !allRoles? {}: allRoles.reduce((acc:{[key:string]:IRole}, item:IRole) => {
-            if (item && item._id) acc[item._id] = item
-            return acc
-        }, {})
-    }
-
-    public async getLeastRole():Promise<IRole|null> {
-        let result:IRole|null = null
-
-        const roles = await this.getAllRoles()
-
-        if (roles) {
-            if (roles.length > 1) {
-                result = roles
-                    .sort((a, b) => {
-                        return b.level - a.level
-                    })[0]
-            } else {
-                result = roles[0]
-            }
-        }
-
-        return result
-    }
-
-    public async getRole(query:IRoleQuery):Promise<IRole|null> {
+    public async getWorkspace(query:{_id:string}):Promise<IWorkspace|null> {
         if (!query._id) return null
         return await this.cachedData.getItem(query._id)
     }
 
-    public async getAllRoles():Promise<IRole[]> {
+    public async getAllWorkspaces():Promise<IWorkspace[]> {
 
-        const result = await this.cachedData.getAllItems<IRole>()
-
-        return result
-    }
-
-    public async getRolesByPage(query:IRoleQuery = {}, pageInfo: IPgeInfo):Promise<IListOutput<IRole>> {
-
-        const result = await this.request.getItemsByPage<IRole>(query, {}, {}, pageInfo)
+        const result = await this.cachedData.getAllItems<IWorkspace>()
 
         return result
     }
 
-    public async saveRole(name:string, level:number, description:string):Promise<IRole | null> {
-        const doc:IRole = {name, level, description}
-        const result = await this.cachedData.createItem<IRole>(doc)
+    public async getWorkspacesByPage(query:any, pageInfo: IPgeInfo):Promise<IListOutput<IWorkspace>> {
+
+        const result = await this.request.getItemsByPage<IWorkspace>(query, {}, {}, pageInfo)
 
         return result
     }
 
-    public async updateRole(id:string, name:string, level:number, description:string):Promise<IRole | null> {
+    public async saveWorkspace(name:string, description:string):Promise<IWorkspace | null> {
+        const doc:IWorkspace = {name, description}
+        const result = await this.cachedData.createItem<IWorkspace>(doc)
+
+        return result
+    }
+
+    public async updateWorkspace(id:string, name:string, level:number, description:string):Promise<IWorkspace | null> {
         const doc:{name?:string, level?:number, description?:string} = {}
 
         if (name) doc.name = name
         if (level) doc.level = level
         if (description) doc.description = description
 
-        const result = await this.cachedData.updateItem<IRole>(id, doc)
+        const result = await this.cachedData.updateItem<IWorkspace>(id, doc)
 
         return result
     }
 
-    public async deleteRole(id:string):Promise<IRole | null> {
+    public async deleteWorkspace(id:string):Promise<IWorkspace | null> {
 
-        const result = await this.cachedData.deleteItem<IRole>(id)
+        const result = await this.cachedData.deleteItem<IWorkspace>(id)
 
         return result
     }
 }
 
-export default new RoleController()
+export default new WorkspaceController()
