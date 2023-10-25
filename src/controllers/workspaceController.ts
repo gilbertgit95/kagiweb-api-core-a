6df325854faf7bf09db958e063ca15867a0ea459
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-10-25 12:24:25 +0800

    update workspace archeticture

--- src/controllers/workspaceController.ts
+++ src/controllers/workspaceController.ts
@@ -18,83 +18,96 @@ class WorkspaceController {
         this.request = new DataRequest(WorkspaceModel)
     }
 
-    public async getWorkspace(isGlobal:boolean=false, currLoggedUser:string, query:{_id:string}):Promise<IWorkspace|null> {
-        if (!query._id) return null
-        const resp = await this.cachedData.getItem<IWorkspace>(query._id)
-        // if not global, then check if the owner is the current logged user else throw error
-        if (!isGlobal && resp && resp.owner !== currLoggedUser) return null
+    public getWorkspace(isAdmin:boolean=false, currLoggedUser:string):(query:{_id:string}) => Promise<IWorkspace|null> {
 
-        return resp
-    }
+        return async (query:{_id:string}) => {
+            if (!query._id) return null
+            const workspace = await this.cachedData.getItem<IWorkspace>(query._id)
+            // if not global, then check if the owner is the current logged user else throw error
+            if (!isAdmin && workspace && workspace.owner !== currLoggedUser) return null
 
-    public async getWorkspacesByPage(isGlobal:boolean=false, currLoggedUser:string, query:any, pageInfo: IPgeInfo):Promise<IListOutput<IWorkspace>> {
-
-        if (!isGlobal) query.owner = currLoggedUser
-        // if not global, then modify the query to only fetch current logged user as owner
-        const result = await this.request.getItemsByPage<IWorkspace>(query, {}, {}, pageInfo)
-
-        return result
-    }
-
-    public async saveWorkspace(isGlobal:boolean=false, currLoggedUser:string, owner:string, name:string, description:string, disabled:string|boolean):Promise<IWorkspace | null> {
-        const createdBy = currLoggedUser
-        const modifiedBy = currLoggedUser
-
-        const doc:IWorkspace = {owner, name, description, createdBy, modifiedBy}
-
-        const user = await userController.getUser({_id: owner})
-        // if not global, then check if the owner is the current logged user else 
-        if (!user) throw({code: 400, message: 'Owner does not exist as a user'})
-
-        const ownerWorkspaces = await WorkspaceModel.find({ owner })
-        if (ownerWorkspaces.length === 0) doc.isActive = true
-
-        // if (DataCleaner.getBooleanData(isActive).isValid) doc.isActive = DataCleaner.getBooleanData(isActive).data
-        if (DataCleaner.getBooleanData(disabled).isValid) doc.disabled = DataCleaner.getBooleanData(disabled).data
-
-        const result = await this.cachedData.createItem<IWorkspace>(doc)
-
-        return result
-    }
-
-    public async updateWorkspace(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string, owner:string, name:string, description:string, disabled:string|boolean):Promise<IWorkspace | null> {
-        if (!isGlobal) {
-            // check if the current user is an owner or has read access to the workspace
-            let workspace = await this.cachedData.getItem<IWorkspace>(workspaceId)
-            if (!workspaceUserController.isCurrUserHasAccess(workspace, currLoggedUser, 'writeAccess')) {
-                throw({code: 403})
-            }
+            return workspace
         }
-
-        const user = await userController.getUser({_id: owner})
-        if (!user) throw({code: 400, message: 'Owner does not exist as a user'})
-
-        const doc:{owner?:string, name?:string, description?:string, modifiedBy?:string, isActive?:boolean, disabled?:boolean} = {}
-
-        if (owner) doc.owner = owner
-        if (name) doc.name = name
-        if (description) doc.description = description
-        if (currLoggedUser) doc.modifiedBy = currLoggedUser
-        // if (DataCleaner.getBooleanData(isActive).isValid) doc.isActive = DataCleaner.getBooleanData(isActive).data
-        if (DataCleaner.getBooleanData(disabled).isValid) doc.disabled = DataCleaner.getBooleanData(disabled).data
-
-        const result = await this.cachedData.updateItem<IWorkspace>(workspaceId, doc)
-
-        return result
     }
 
-    public async deleteWorkspace(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string):Promise<IWorkspace | null> {
-        const workspace = await WorkspaceModel.findOne({_id: workspaceId})
-        if (!workspace) throw({code: 404}) // Resource not found
-        if (workspace.isActive) throw({code: 403, message: 'Cannot delete if workspace tobe deleted is active'})
+    public getWorkspacesByPage(isAdmin:boolean=false, currLoggedUser:string):(query:any, pageInfo: IPgeInfo) => Promise<IListOutput<IWorkspace>> {
 
-        const ownerWorkspaces = await WorkspaceModel.find({owner: workspace.owner})
-        if (ownerWorkspaces.length === 1) throw({code: 403, message: 'Cannot delete if the owner has only 1 workspace'}) // Forbidden access to resources.
+        return async (query:any, pageInfo: IPgeInfo) => {
+            if (!isAdmin) query.owner = currLoggedUser
+            // if not global, then modify the query to only fetch current logged user as owner
+            const result = await this.request.getItemsByPage<IWorkspace>(query, {}, {}, pageInfo)
 
-        const result = await this.cachedData.deleteItem<IWorkspace>(workspaceId)
-
-        return result
+            return result
+        }
     }
+
+    public saveWorkspace(isAdmin:boolean=false, currLoggedUser:string):(owner:string, name:string, description:string, disabled:string|boolean) => Promise<IWorkspace | null> {
+        return async (owner:string, name:string, description:string, disabled:string|boolean) => {
+            const createdBy = currLoggedUser
+            const modifiedBy = currLoggedUser
+
+            const doc:IWorkspace = {owner, name, description, createdBy, modifiedBy}
+
+            const user = await userController.getUser({_id: owner})
+            // if not global, then check if the owner is the current logged user else 
+            if (!user) throw({code: 400, message: 'Owner does not exist as a user'})
+
+            const ownerWorkspaces = await WorkspaceModel.find({ owner })
+            if (ownerWorkspaces.length === 0) doc.isActive = true
+
+            // if (DataCleaner.getBooleanData(isActive).isValid) doc.isActive = DataCleaner.getBooleanData(isActive).data
+            if (DataCleaner.getBooleanData(disabled).isValid) doc.disabled = DataCleaner.getBooleanData(disabled).data
+
+            const result = await this.cachedData.createItem<IWorkspace>(doc)
+
+            return result
+        }
+    }
+
+    public updateWorkspace(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string, owner:string, name:string, description:string, disabled:string|boolean) => Promise<IWorkspace | null> {
+        return async (workspaceId:string, owner:string, name:string, description:string, disabled:string|boolean) => {
+            if (!isAdmin) {
+                // check if the current user is an owner or has read access to the workspace
+                let workspace = await this.cachedData.getItem<IWorkspace>(workspaceId)
+                if (!workspaceUserController.isCurrUserHasAccess(workspace, currLoggedUser, 'writeAccess')) {
+                    throw({code: 403})
+                }
+            }
+    
+            const user = await userController.getUser({_id: owner})
+            if (!user) throw({code: 400, message: 'Owner does not exist as a user'})
+    
+            const doc:{owner?:string, name?:string, description?:string, modifiedBy?:string, isActive?:boolean, disabled?:boolean} = {}
+    
+            if (owner) doc.owner = owner
+            if (name) doc.name = name
+            if (description) doc.description = description
+            if (currLoggedUser) doc.modifiedBy = currLoggedUser
+            // if (DataCleaner.getBooleanData(isActive).isValid) doc.isActive = DataCleaner.getBooleanData(isActive).data
+            if (DataCleaner.getBooleanData(disabled).isValid) doc.disabled = DataCleaner.getBooleanData(disabled).data
+    
+            const result = await this.cachedData.updateItem<IWorkspace>(workspaceId, doc)
+    
+            return result
+        }
+    }
+
+    public deleteWorkspace(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string) => Promise<IWorkspace | null> {
+
+        return async (workspaceId:string) => {
+            const workspace = await WorkspaceModel.findOne({_id: workspaceId})
+            if (!workspace) throw({code: 404}) // Resource not found
+            if (workspace.isActive) throw({code: 403, message: 'Cannot delete if workspace tobe deleted is active'})
+
+            const ownerWorkspaces = await WorkspaceModel.find({owner: workspace.owner})
+            if (ownerWorkspaces.length === 1) throw({code: 403, message: 'Cannot delete if the owner has only 1 workspace'}) // Forbidden access to resources.
+
+            const result = await this.cachedData.deleteItem<IWorkspace>(workspaceId)
+
+            return result
+        }
+    }
+
 }
 
 export default new WorkspaceController()
--- src/controllers/workspaceUserController.ts
+++ src/controllers/workspaceUserController.ts
@@ -50,99 +50,113 @@ class WorkspaceUsersController {
         return null
     }
 
-    public async getUserRef(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string, userRefId:string):Promise<IUserRef|null> {
-        if (!(workspaceId && userRefId)) throw({code: 400})
+    public getUserRef(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string, userRefId:string) => Promise<IUserRef|null> {
+        return async (workspaceId:string, userRefId:string) => {
+            if (!(workspaceId && userRefId)) throw({code: 400})
 
-        const workspace = await workspaceController.getWorkspace(isGlobal, currLoggedUser, {_id: workspaceId})
-        if (!workspace) throw({code: 404})
+            const workspace = await workspaceController.getWorkspace(isAdmin, currLoggedUser)({_id: workspaceId})
+            if (!workspace) throw({code: 404})
 
-        const userRef = this.getUserRefByRefId(workspace, userRefId)
-        if (!userRef) throw({code: 404})
+            const userRef = this.getUserRefByRefId(workspace, userRefId)
+            if (!userRef) throw({code: 404})
 
-        return userRef
-    }
-
-    public async getUserRefs(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string):Promise<IUserRef[]> {
-        let result:IUserRef[] = []
-        if (!workspaceId) throw({code: 400})
-
-        const workspace = await workspaceController.getWorkspace(isGlobal, currLoggedUser, {_id: workspaceId})
-        if (!workspace) throw({code: 404})
-        result = workspace!.usersRefs? workspace!.usersRefs: []
-
-        return result
-    }
-
-    public async saveUserRef(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string, userId:string, readAccess:string|boolean, writeAccess:string|boolean):Promise<IUserRef|null> {
-        if (!(workspaceId && userId)) throw({code: 400})
-
-        const workspace = await WorkspaceModel.findOne({_id: workspaceId})
-        if (!workspace) throw({code: 404})
-
-        const user = await userController.getUser({_id: userId})
-        // check if workspace existed on workspaces collection
-        if (!user) throw({code: 404})
-
-        // check if the workspace to update is existing on the workspace workspaces refs
-        if (this.hasUser(workspace, userId)) throw({code: 409})
-        const doc:{userId?:string, readAccess?:boolean, writeAccess?:boolean} = {userId}
-        if (DataCleaner.getBooleanData(readAccess).isValid) doc.readAccess = DataCleaner.getBooleanData(readAccess).data
-        if (DataCleaner.getBooleanData(writeAccess).isValid) doc.writeAccess = DataCleaner.getBooleanData(writeAccess).data
-
-        workspace.usersRefs!.push(doc)
-        workspace.modifiedBy = currLoggedUser
-
-        await workspace.save()
-        await workspaceController.cachedData.removeCacheData(workspaceId)
-
-        return this.getUserRefByUserId(workspace, userId)
-    }
-
-    public async updateUserRef(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string, userRefId:string, userId:string, readAccess:string|boolean, writeAccess:string|boolean):Promise<IUserRef|null> {
-        if (!(workspaceId && userRefId && userId)) throw({code: 400})
-
-        const workspace = await WorkspaceModel.findOne({_id: workspaceId})
-        if (!workspace) throw({code: 404})
-        if (!workspace.usersRefs?.id(userRefId)) throw({code: 404})
-
-        const user = await userController.getUser({_id: userId})
-        // check if workspace existed on workspaces collection
-        if (!user) throw({code: 404})
-
-        // check if the workspace to update is existing on the workspace workspaces refs
-        if (this.hasUser(workspace, userId)) throw({code: 409})
-
-        workspace.usersRefs!.id(userRefId)!.userId = userId
-        if (DataCleaner.getBooleanData(readAccess).isValid) {
-            workspace.usersRefs!.id(userRefId)!.readAccess = DataCleaner.getBooleanData(readAccess).data
+            return userRef
         }
-        if (DataCleaner.getBooleanData(writeAccess).isValid) {
-            workspace.usersRefs!.id(userRefId)!.writeAccess = DataCleaner.getBooleanData(writeAccess).data
-        }
-        workspace.modifiedBy = currLoggedUser
-
-        await workspace.save()
-        await workspaceController.cachedData.removeCacheData(workspaceId)
-
-        return workspace.usersRefs!.id(userRefId)
     }
 
-    public async deleteUserRef(isGlobal:boolean=false, currLoggedUser:string, workspaceId:string, userRefId:string):Promise<IUserRef|null> {
-        if (!(workspaceId && userRefId)) throw({code: 400})
+    public getUserRefs(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string) => Promise<IUserRef[]> {
 
-        const workspace = await WorkspaceModel.findOne({_id: workspaceId})
-        if (!workspace) throw({code: 404})
+        return async (workspaceId:string) => {
+            let result:IUserRef[] = []
+            if (!workspaceId) throw({code: 400})
 
-        const userRefData = workspace!.usersRefs?.id(userRefId)
-        if (userRefData) {
-            workspace!.usersRefs?.id(userRefId)?.deleteOne()
+            const workspace = await workspaceController.getWorkspace(isAdmin, currLoggedUser)({_id: workspaceId})
+            if (!workspace) throw({code: 404})
+            result = workspace!.usersRefs? workspace!.usersRefs: []
+
+            return result
+        }
+    }
+
+    public saveUserRef(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string, userId:string, readAccess:string|boolean, writeAccess:string|boolean) => Promise<IUserRef|null> {
+        
+        return async (workspaceId:string, userId:string, readAccess:string|boolean, writeAccess:string|boolean) => {
+            if (!(workspaceId && userId)) throw({code: 400})
+
+            const workspace = await WorkspaceModel.findOne({_id: workspaceId})
+            if (!workspace) throw({code: 404})
+
+            const user = await userController.getUser({_id: userId})
+            // check if workspace existed on workspaces collection
+            if (!user) throw({code: 404})
+
+            // check if the workspace to update is existing on the workspace workspaces refs
+            if (this.hasUser(workspace, userId)) throw({code: 409})
+            const doc:{userId?:string, readAccess?:boolean, writeAccess?:boolean} = {userId}
+            if (DataCleaner.getBooleanData(readAccess).isValid) doc.readAccess = DataCleaner.getBooleanData(readAccess).data
+            if (DataCleaner.getBooleanData(writeAccess).isValid) doc.writeAccess = DataCleaner.getBooleanData(writeAccess).data
+
+            workspace.usersRefs!.push(doc)
+            workspace.modifiedBy = currLoggedUser
+
             await workspace.save()
             await workspaceController.cachedData.removeCacheData(workspaceId)
-        } else {
-            throw({code: 404})
-        }
 
-        return userRefData? userRefData: null
+            return this.getUserRefByUserId(workspace, userId)
+        }
+    }
+
+    public updateUserRef(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string, userRefId:string, userId:string, readAccess:string|boolean, writeAccess:string|boolean) => Promise<IUserRef|null> {
+        
+        return async (workspaceId:string, userRefId:string, userId:string, readAccess:string|boolean, writeAccess:string|boolean) => {
+            if (!(workspaceId && userRefId && userId)) throw({code: 400})
+
+            const workspace = await WorkspaceModel.findOne({_id: workspaceId})
+            if (!workspace) throw({code: 404})
+            if (!workspace.usersRefs?.id(userRefId)) throw({code: 404})
+
+            const user = await userController.getUser({_id: userId})
+            // check if workspace existed on workspaces collection
+            if (!user) throw({code: 404})
+
+            // check if the workspace to update is existing on the workspace workspaces refs
+            if (this.hasUser(workspace, userId)) throw({code: 409})
+
+            workspace.usersRefs!.id(userRefId)!.userId = userId
+            if (DataCleaner.getBooleanData(readAccess).isValid) {
+                workspace.usersRefs!.id(userRefId)!.readAccess = DataCleaner.getBooleanData(readAccess).data
+            }
+            if (DataCleaner.getBooleanData(writeAccess).isValid) {
+                workspace.usersRefs!.id(userRefId)!.writeAccess = DataCleaner.getBooleanData(writeAccess).data
+            }
+            workspace.modifiedBy = currLoggedUser
+
+            await workspace.save()
+            await workspaceController.cachedData.removeCacheData(workspaceId)
+
+            return workspace.usersRefs!.id(userRefId)
+        }
+    }
+
+    public deleteUserRef(isAdmin:boolean=false, currLoggedUser:string):(workspaceId:string, userRefId:string) => Promise<IUserRef|null> {
+
+        return async (workspaceId:string, userRefId:string) => {
+            if (!(workspaceId && userRefId)) throw({code: 400})
+
+            const workspace = await WorkspaceModel.findOne({_id: workspaceId})
+            if (!workspace) throw({code: 404})
+
+            const userRefData = workspace!.usersRefs?.id(userRefId)
+            if (userRefData) {
+                workspace!.usersRefs?.id(userRefId)?.deleteOne()
+                await workspace.save()
+                await workspaceController.cachedData.removeCacheData(workspaceId)
+            } else {
+                throw({code: 404})
+            }
+
+            return userRefData? userRefData: null
+        }
     }
 }
 
