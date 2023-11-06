f9fe26f0e6261e2fd8df0739bbe1d3f3eb46c3a9
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-11-06 20:06:20 +0800

    update valuidators

--- src/dataSource/validators/textValidators.js
+++ src/dataSource/validators/textValidators.js
@@ -1,7 +1,27 @@
-// validate living thing names
+import { phone } from 'phone'
 
+// validate living thing names
+const validateLivingNames = {
+
+}
 // validate object names
+const validateObjectNames = {
+    
+}
 
 // validate email
+const validateEmail = {
+    
+}
 
 // validate phone numbers
+const validatePhone = {
+    
+}
+
+export default {
+    validateLivingNames,
+    validateObjectNames,
+    validateEmail,
+    validatePhone
+}
