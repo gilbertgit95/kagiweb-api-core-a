c78dea69147a70e83e52d34099639d0dfe00a000
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-11-12 01:02:11 +0800

    update text data validators

--- src/dataSource/validators/textValidators.ts
+++ src/dataSource/validators/textValidators.ts
@@ -5,27 +5,33 @@ class TextValidators {
     // validate living thing names
     public static validateLivingNames = {
         validator: (value:string):boolean => {
-            return /\d{3}-\d{3}-\d{4}/.test(value)
+            return /[a-zA-Z]+/.test(value)
         },
         message: (props:{value:string}):string => `${props.value} is not a valid name!`
     }
 
     // validate object names
     public static validateObjectNames = {
-        
+        validator: (value:string):boolean => {
+            return /[a-zA-Z]+[0-9?a-zA-Z]+/.test(value)
+        },
+        message: (props:{value:string}):string => `${props.value} is not a valid name!`
     }
 
     // validate description
     public static validateDescription = {
         validator: (value:string):boolean => {
-            return /\d{3}-\d{3}-\d{4}/.test(value)
+            return /^(.|\s)*[a-zA-Z]+(.|\s)*$/.test(value)
         },
         message: (props:{value:string}):string => `${props.value} is not a valid description!`
     }
 
     // validate email
     public static validateEmail = {
-        
+        validator: (value:string):boolean => {
+            return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)
+        },
+        message: (props:{value:string}):string => `${props.value} is not a valid email!`
     }
 
     // validate phone numbers
