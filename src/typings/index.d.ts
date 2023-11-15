c9428b1040054ca3c07dd864f3f79e388646167e
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-11-16 02:41:47 +0800

    init typings dir

---
+++ src/typings/index.d.ts
@@ -0,0 +1,8 @@
+import {IUser} from '../dataSource/models/userModel'
+import {IWorkspace} from '../dataSource/models/workspaceModel'
+
+export namespace Express {
+    interface Request {
+        
+    }
+}
