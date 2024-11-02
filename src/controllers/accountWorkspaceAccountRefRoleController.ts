cbd1911c9878ee62d843c42c4daedd72a93b31c9
gilbert cuerbo <gilbert.cuerbo@gmail.com>
2024-11-02 20:50:06 +0800

    update controller AccountWorkspaceAccountRefRoleController

--- src/controllers/accountWorkspaceAccountRefRoleController.ts
+++ src/controllers/accountWorkspaceAccountRefRoleController.ts
@@ -1,4 +1,6 @@
 import accountModel, { IAccount, IRoleRef } from '../dataSource/models/accountModel'
+import accountWorkspaceAccountRefController from './accountWorkspaceAccountRefController'
+
 import accountController from './accountController'
 import roleController from './roleController'
 import DataCleaner from '../utilities/dataCleaner'
@@ -7,9 +9,10 @@ import DataCleaner from '../utilities/dataCleaner'
 // const env = Config.getEnv()
 
 class AccountWorkspaceAccountRefRoleController {
-    public hasRole(account:IAccount, roleId:string):boolean {
-        if (account && account.rolesRefs) {
-            for (const ref of account.rolesRefs) {
+    public hasRole(account:IAccount, workspaceId:string, accountRefId:string, roleId:string):boolean {
+        const accRef = accountWorkspaceAccountRefController.getWorkspaceAccountRefById(account, workspaceId, accountRefId)
+        if (accRef && accRef.rolesRefs) {
+            for (const ref of accRef.rolesRefs) {
                 if (ref.roleId === roleId) return true
             }
         }
@@ -17,10 +20,10 @@ class AccountWorkspaceAccountRefRoleController {
         return false
     }
 
-    public getRoleRefByRoleId(account:IAccount, roleId:string):IRoleRef|null {
-
-        if (account && account.rolesRefs) {
-            for (const ref of account.rolesRefs) {
+    public getRoleRefByRoleId(account:IAccount, workspaceId:string, accountRefId:string, roleId:string):IRoleRef|null {
+        const accRef = accountWorkspaceAccountRefController.getWorkspaceAccountRefById(account, workspaceId, accountRefId)
+        if (accRef && accRef.rolesRefs) {
+            for (const ref of accRef.rolesRefs) {
                 if (ref.roleId === roleId) return ref
             }
         }
@@ -28,10 +31,10 @@ class AccountWorkspaceAccountRefRoleController {
         return null
     }
 
-    public async getRoleRefByRefId(account:IAccount, roleRefId:string):Promise<IRoleRef|null> {
-
-        if (account && account.rolesRefs) {
-            for (const ref of account.rolesRefs) {
+    public async getRoleRefByRefId(account:IAccount, workspaceId:string, accountRefId:string, roleRefId:string):Promise<IRoleRef|null> {
+        const accRef = accountWorkspaceAccountRefController.getWorkspaceAccountRefById(account, workspaceId, accountRefId)
+        if (accRef && accRef.rolesRefs) {
+            for (const ref of accRef.rolesRefs) {
                 if (ref._id === roleRefId) return ref
             }
         }
@@ -39,29 +42,31 @@ class AccountWorkspaceAccountRefRoleController {
         return null
     }
 
-    public async getRoleRef(accountId:string, roleRefId:string):Promise<IRoleRef|null> {
-        if (!(accountId && roleRefId)) throw({code: 400})
+    public async getRoleRef(accountId:string, workspaceId:string, accountRefId:string, roleRefId:string):Promise<IRoleRef|null> {
+        if (!(accountId && workspaceId && accountRefId && roleRefId)) throw({code: 400})
 
         const account = await accountController.getAccount({_id: accountId})
         if (!account) throw({code: 404})
 
-        const roleRef = this.getRoleRefByRefId(account, roleRefId)
+        const roleRef = this.getRoleRefByRefId(account, workspaceId, accountRefId, roleRefId)
         if (!roleRef) throw({code: 404})
 
         return roleRef
     }
 
-    public async getRoleRefs(accountId:string):Promise<IRoleRef[]> {
-        if (!accountId) throw({code: 400})
+    public async getRoleRefs(accountId:string, workspaceId:string, accountRefId:string):Promise<IRoleRef[]> {
+        if (!accountId && workspaceId && accountRefId) throw({code: 400})
 
         const account = await accountController.getAccount({_id: accountId})
         if (!account) throw({code: 404})
+
+        const accRef = accountWorkspaceAccountRefController.getWorkspaceAccountRefById(account, workspaceId, accountRefId)
         
-        return account!.rolesRefs? account!.rolesRefs: []
+        return accRef!.rolesRefs? accRef!.rolesRefs: []
     }
 
-    public async saveRoleRef(accountId:string, roleId:string):Promise<IRoleRef|null> {
-        if (!(accountId && roleId)) throw({code: 400})
+    public async saveRoleRef(accountId:string, workspaceId:string, accountRefId:string, roleId:string):Promise<IRoleRef|null> {
+        if (!(accountId && workspaceId && accountRefId && roleId)) throw({code: 400})
 
         const account = await accountModel.findOne({_id: accountId})
         if (!account) throw({code: 404})
@@ -70,22 +75,22 @@ class AccountWorkspaceAccountRefRoleController {
         // check if role existed on roles collection
         if (!rolesMap[roleId]) throw({code: 404})
         // check if the role to update is existing on the account role refs
-        if (this.hasRole(account, roleId)) throw({code: 409})
+        if (this.hasRole(account, workspaceId, accountRefId, roleId)) throw({code: 409})
         const doc:{roleId:string, isActive?:boolean} = {roleId}
 
-        account.rolesRefs!.push(doc)
+        account.workspaces.id(workspaceId)?.accountRefs?.id(accountRefId)?.rolesRefs?.push(doc)
         await account.save()
         await accountController.cachedData.removeCacheData(accountId)
 
-        return this.getRoleRefByRoleId(account, roleId)
+        return this.getRoleRefByRoleId(account, workspaceId, accountRefId, roleId)
     }
 
-    public async updateRoleRef(accountId:string, roleRefId:string, roleId:string|undefined):Promise<IRoleRef|null> {
-        if (!(accountId && roleRefId)) throw({code: 400})
+    public async updateRoleRef(accountId:string, workspaceId:string, accountRefId:string, roleRefId:string, roleId:string|undefined):Promise<IRoleRef|null> {
+        if (!(accountId && workspaceId && accountRefId && roleRefId && roleId)) throw({code: 400})
 
         const account = await accountModel.findOne({_id: accountId})
         if (!account) throw({code: 404})
-        if (!account.rolesRefs?.id(roleRefId)) throw({code: 404})
+        if (!account.workspaces.id(workspaceId)?.accountRefs?.id(accountRefId)?.rolesRefs?.id(roleRefId)) throw({code: 404})
 
         if (roleId) {
             const rolesMap = await roleController.getRolesMap()
@@ -93,9 +98,9 @@ class AccountWorkspaceAccountRefRoleController {
             if (!rolesMap[roleId]) throw({code: 404})
 
             // check if the role to update is existing on the role roles refs
-            if (this.hasRole(account, roleId)) throw({code: 409})
+            if (this.hasRole(account, workspaceId, accountRefId, roleId)) throw({code: 409})
 
-            account.rolesRefs!.id(roleRefId)!.roleId = roleId
+            account.workspaces.id(workspaceId)!.accountRefs!.id(accountRefId)!.rolesRefs!.id(roleRefId)!.roleId = roleId
         }
 
         await account.save()
@@ -104,18 +109,20 @@ class AccountWorkspaceAccountRefRoleController {
         return account.rolesRefs!.id(roleRefId)
     }
 
-    public async deleteRoleRef(accountId:string, roleRefId:string):Promise<IRoleRef|null> {
-        if (!(accountId && roleRefId)) throw({code: 400})
+    public async deleteRoleRef(accountId:string, workspaceId:string, accountRefId:string, roleRefId:string):Promise<IRoleRef|null> {
+        if (!(accountId && workspaceId && accountRefId && roleRefId)) throw({code: 400})
 
         const account = await accountModel.findOne({_id: accountId})
         if (!account) throw({code: 404})
 
+        const accRef = accountWorkspaceAccountRefController.getWorkspaceAccountRefById(account, workspaceId, accountRefId)
         // if account has only one role, then do not delete the role
-        if (account.rolesRefs.length <= 1) throw({code: 409})
+        if (!accRef) throw({code: 404})
+        if (accRef?.rolesRefs && accRef.rolesRefs.length <= 1) throw({code: 409})
 
-        const roleRefData = account!.rolesRefs?.id(roleRefId)
+        const roleRefData = account.workspaces.id(workspaceId)!.accountRefs!.id(accountRefId)!.rolesRefs!.id(roleRefId)
         if (roleRefData) {
-            account!.rolesRefs?.id(roleRefId)?.deleteOne()
+            account.workspaces.id(workspaceId)!.accountRefs!.id(accountRefId)!.rolesRefs!.id(roleRefId)?.deleteOne()
             await account.save()
             await accountController.cachedData.removeCacheData(accountId)
         } else {
