d27f2de018b89631d2990af9ec85dca20cec7c08
gilbert cuerbo <gilbert.cuerbo@gmail.com>
2024-11-07 14:11:42 +0800

    update defaults generators

--- src/utilities/defaultsData.ts
+++ src/utilities/defaultsData.ts
@@ -12,7 +12,10 @@ export const generateDefaultRoleRefs = () => {
 }
 
 export const generateDefaultAccountConfigs = () => {
-    return []
+    return [
+        { key: 'default-role', value: '', type: 'string' },
+        { key: 'default-workspace', value: '', type: 'string' }
+    ]
 }
 
 export const generateDefaultPasswords = () => {
@@ -31,25 +34,12 @@ export const generateDefaultAccountData = ():any => {
     return { // eslint-disable-line @typescript-eslint/no-explicit-any
         nameId: null,
         rolesRefs: [],
-        accountInfos: [
-            { key: 'firstname', value: '', type: 'string' },
-            { key: 'middlename', value: '', type: 'string' },
-            { key: 'lastname', value: '', type: 'string' },
-            { key: 'birthday', value: '', type: 'date' },
-        ],
-        accountConfigs: [
-            { key: 'default-role', value: '', type: 'string' },
-            { key: 'default-workspace', value: '', type: 'string' }
-        ],
-        passwords: [],
+        accountInfos:        generateDefaultAccountsInfo(),
+        accountConfigs:      generateDefaultAccountConfigs(),
+        passwords:           generateDefaultPasswords(),
         contactInfos: [],
         clientDevices: [],
-        limitedTransactions: [
-            { limit: 5, type: 'signin' },
-            { limit: 5, type: 'otp-signin', disabled: true },
-            { limit: 5, type: 'forgot-pass' },
-            { limit: 5, type: 'reset-pass' },
-            { limit: 5, type: 'verify-contact'}
-        ]
+        limitedTransactions: generateDefaultLimitedTransactions(),
+        workspaces:          generateDefaultWorkspaces()
     }
 }
