7ae31f7eeecfa8a8b44661f12d041fe9c2f07fea
gilbert cuerbo <gilbert.cuerbo@gmail.com>
2024-11-19 20:33:58 +0800

    remove validation in features name for now

--- src/dataSource/models/featureModel.ts
+++ src/dataSource/models/featureModel.ts
@@ -41,8 +41,7 @@ const FeatureSchema = new Schema<IFeature>({
     name: {
         type: String,
         required: false,
-        default: '',
-        validate: TextValidators.validateFlexibleName
+        default: ''
     },
     type: {
         type: String,
