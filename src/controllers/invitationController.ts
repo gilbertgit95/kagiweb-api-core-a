bf2b95c1321a1fd7cd63345e695893a553544e8a
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2025-03-02 23:07:22 +0800

    update process comments

--- src/controllers/invitationController.ts
+++ src/controllers/invitationController.ts
@@ -1,17 +1,49 @@
 class InvitationController {
     public async acceptAccountInvitation(accountId:string, toAccountId:string):Promise<boolean> {
+        // get invited account data
+
+        // get account invitation is comming from
+
+        // check if the invitation was sent
+
+        // update the accoumt reference so that accepeted field is true and decliend remains false
         return true
     }
 
     public async declineAccountInvitation(accountId:string, toAccountId:string):Promise<boolean> {
+        // get invited account data
+
+        // get account invitation is comming from
+
+        // check if the invitation was sent
+
+        // update the accoumt reference so that declined field is true and accepted remains false
         return true
     }
 
     public async acceptAccountWorkspaceInvitation(accountId:string, toAccountId:string, toWorkspaceId:string):Promise<boolean> {
+        // get invited account data
+
+        // get account invitation is comming from
+
+        // check workspace is existing
+
+        // check if the invitation was sent
+
+        // update the accoumt reference so that decliend field is true and accepted remains false
         return true
     }
 
     public async declineAccountWorkspaceInvitation(accountId:string, toAccountId:string, toWorkspaceId:string):Promise<boolean> {
+        // get invited account data
+
+        // get account invitation is comming from
+
+        // check workspace is existing
+
+        // check if the invitation was sent
+
+        // update the accoumt reference so that declined field is true and accepted remains false
         return true
     }
 }
