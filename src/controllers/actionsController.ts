e9f7089e1e7f36cd65f261f215ebeaef6db971f1
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2025-03-22 21:06:13 +0800

    init update on action info getter

--- src/controllers/actionsController.ts
+++ src/controllers/actionsController.ts
@@ -1,6 +1,54 @@
+import accountModel, { IWorkspace } from '../dataSource/models/accountModel'
+
 class ActionsController {
     // accounts/:accountId/actions/:actionType/module/:moduleType/:moduleId/ref/:refType/:refId
     public async getAccountActionInfo(accountId:string, actionType:string, moduleType:string, moduleId:string, refType:string, refId:string):Promise<boolean> {
+        // invitations
+        if (actionType === 'invitation') {
+            // if moduleType is account get account invitation
+            if (moduleType === 'account') {
+                const extWorkspaces:(IWorkspace & {ownerId:string, ownerNameId: string, ownerAccountType: string})[] = await accountModel.aggregate<IWorkspace & {ownerId:string, ownerNameId: string, ownerAccountType: string}>([
+                    {
+                      '$match': {
+                        'workspaces.accountRefs.accountId': accountId
+                      }
+                    }, {
+                      '$unwind': {
+                        'path': '$workspaces', 
+                        'preserveNullAndEmptyArrays': false
+                      }
+                    }, {
+                      '$addFields': {
+                        'account': '$workspaces.accountRefs'
+                      }
+                    }, {
+                      '$unwind': {
+                        'path': '$account', 
+                        'preserveNullAndEmptyArrays': false
+                      }
+                    }, {
+                      '$match': {
+                        'account.accountId': accountId
+                      }
+                    }, {
+                      '$project': {
+                        '_id': '$workspaces._id',
+                        'name': '$workspaces.name',
+                        'description': '$workspaces.description',
+                        'ownerId': '$_id',
+                        'ownerNameId': '$nameId',
+                        'ownerAccountType': '$accountType',
+                        'isActive': '$workspaces.isActive',
+                        'accountRefs': '$workspaces.accountRefs',
+                        'createdAt': '$workspaces.createdAt',
+                        'updatedAt': '$workspaces.updatedAt',
+                        'disabled': '$workspaces.disabled'
+                      }
+                    }
+                ])
+            }
+        }
+
         return true
     }
 
