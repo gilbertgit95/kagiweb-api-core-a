fc8b64d37c9357795ab87c52c3064bb3cdb88e55
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2025-03-25 22:51:39 +0800

    update aggregation

--- src/controllers/actionsController.ts
+++ src/controllers/actionsController.ts
@@ -1,4 +1,19 @@
-import accountModel, { IWorkspace } from '../dataSource/models/accountModel'
+import accountModel, { IWorkspace, IAccountAccountRef } from '../dataSource/models/accountModel'
+import { IRole } from '../dataSource/models/roleModel'
+
+interface IAccountActionInfo {
+    _id: string,
+    fromAccount: {
+        _id: string,
+        nameId: string
+    },
+    toAccount: {
+        _id: string,
+        nameId: string
+    },
+    ref: IAccountAccountRef,
+    refRole: IRole
+}
 
 class ActionsController {
     // accounts/:accountId/actions/:actionType/module/:moduleType/:moduleId/ref/:refType/:refId
@@ -11,61 +26,78 @@ class ActionsController {
                 // get the the account reference data
                 // get the account reference role
 
-                const extWorkspaces:(IWorkspace & {ownerId:string, ownerNameId: string, ownerAccountType: string})[] = await accountModel.aggregate<IWorkspace & {ownerId:string, ownerNameId: string, ownerAccountType: string}>([
-                        {
-                          $match: {
-                            'accountRefs.accountId':
-                              '37410e75-1760-4bb6-85e0-d0a138d374bc'
-                          }
-                        },
-                        {
-                          $unwind: {
-                            path: '$accountRefs',
-                            preserveNullAndEmptyArrays: false
-                          }
-                        },
-                        {
-                          $match: {
-                            'accountRefs.accountId':
-                              '37410e75-1760-4bb6-85e0-d0a138d374bc'
-                          }
-                        },
-                        {
-                          $unwind: {
-                            path: '$accountRefs.accountConfigs',
-                            preserveNullAndEmptyArrays: false
-                          }
-                        },
-                        {
-                          $match: {
-                            'accountRefs.accountConfigs.key':
-                              'default-role'
-                          }
-                        },
-                        {
-                          $lookup: {
-                            from: 'roles',
-                            localField:
-                              'accountRefs.accountConfigs.value',
-                            foreignField: '_id',
-                            as: 'accountAccountRole'
-                          }
-                        },
-                        {
-                          $unwind: {
-                            path: '$accountAccountRole',
-                            preserveNullAndEmptyArrays: false
-                          }
-                        },
-                        {
-                          $project: {
-                            'toAccount._id': '$_id',
-                            'toAccount.nameId': '$nameId',
-                            accountRef: 1
-                          }
-                        }
-                      ],
-                    );
+                const actionInfo:IAccountActionInfo[] = await accountModel.aggregate<IAccountActionInfo>([
+                  {
+                    $match: {
+                      "accountRefs.accountId":
+                        "37410e75-1760-4bb6-85e0-d0a138d374bc"
+                    }
+                  },
+                  {
+                    $unwind: {
+                      path: "$accountRefs",
+                      preserveNullAndEmptyArrays: false
+                    }
+                  },
+                  {
+                    $match: {
+                      "accountRefs.accountId":
+                        "37410e75-1760-4bb6-85e0-d0a138d374bc"
+                    }
+                  },
+                  {
+                    $unwind: {
+                      path: "$accountRefs.accountConfigs",
+                      preserveNullAndEmptyArrays: false
+                    }
+                  },
+                  {
+                    $match: {
+                      "accountRefs.accountConfigs.key":
+                        "default-role"
+                    }
+                  },
+                  {
+                    $lookup: {
+                      from: "roles",
+                      localField:
+                        "accountRefs.accountConfigs.value",
+                      foreignField: "_id",
+                      as: "accountAccountRole"
+                    }
+                  },
+                  {
+                    $unwind: {
+                      path: "$accountAccountRole",
+                      preserveNullAndEmptyArrays: false
+                    }
+                  },
+                  {
+                    $lookup:
+                      {
+                        from: "accounts",
+                        localField: "accountRefs.accountId",
+                        foreignField: "_id",
+                        as: "fromAccount"
+                      }
+                  },
+                  {
+                    $unwind:
+                      {
+                        path: "$fromAccount",
+                        preserveNullAndEmptyArrays: false
+                      }
+                  },
+                  {
+                    $project: {
+                      "toAccount._id": "$_id",
+                      "toAccount.nameId": "$nameId",
+                      "fromAccount._id": "$fromAccount._id",
+                      "fromAccount.nameId": "$fromAccount.nameId",
+                      ref: "$accountRefs",
+                      refRole: "$accountAccountRole"
+                    }
+                  }
                 ])
             }
         }
